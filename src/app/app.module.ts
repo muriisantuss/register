9dbb83266c2318b653162d4fb14accbaf73d8508
muriisantuss <muriisantuss@gmail.com>
2024-03-18 20:03:49 -0300

    delete 'students' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { StudentsComponent } from './students/students.component';
 import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent,
     ProductComponent
   ],
   imports: [
